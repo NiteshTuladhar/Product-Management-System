39e574bd6a74895db320a01064a0ff76c063b5e3
Nitesh <niteshtuladhar10@gmail.com>
2025-10-15 08:53:50 +0545

    feat(common) : add an interceptor for unified API response format.
    - This enables consistent response structure across all endpoints

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TransformInterceptor } from './common/interceptors/renderer.interceptors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 }
+
+console.log(`Local server started on port`, process.env.PORT ?? 3000);
 bootstrap();
